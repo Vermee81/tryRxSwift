48bb223e6a8e8de19f61694223cca9f59a787d6b
Vermee81 <hrksb5029@gmail.com>
2019-05-06 17:46:03 +0900

    [init] Initial commit

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'tryRxSwift' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for tryRxSwift
+  pod 'RxSwift', '~> 4.5'
+  pod 'RxCocoa', '~> 4.5'
+
+  target 'tryRxSwiftTests' do
+    inherit! :search_paths
+    # Pods for testing
+    pod 'RxTest', '~> 4.5'
+  end
+
+end
---
+++ tryRxSwift/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  tryRxSwift
+//
+//  Created by Hiroo Kusaba on 2019/05/06.
+//  Copyright © 2019年 Hiroo Kusaba. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let observable = Observable.of(
+        "R",
+        "Rx",
+        "RxS",
+        "RxSw",
+        "RxSwi",
+        "RxSwif",
+        "RxSwift")
+        
+        _ = observable
+            .filter {$0.count >= 2}
+            .map {$0.lowercased()}
+            .subscribe(onNext:{
+            print("onNext: ", $0)
+        },
+                                 onCompleted:{
+                                    print("Done")
+        })
+        
+    }
+
+
+}
+
